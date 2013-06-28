17ef5789d1fcf082481738f67171564ec5212a03
Lawrence Klyne <githubatlklyne.co.uk>
2013-06-28 09:28:39 +0100

    Build for STm32f10X, The value line discovery board. Let linker manage RAM, so easier to combine forth with other code. This changed the defvar macro to reserver space in a data section, named the text section .text.forth and tweaked the linker file to load the correct sections.

---
+++ stm32f100/stm32f100_words.s
@@ -0,0 +1,26 @@
+    .ltorg
+
+    defcode "RETI", RETI
+    pop {r4 - r12, pc}
+
+    defword ";I", SEMICOLONI, F_IMMED
+    .word LIT, RETI, COMMAXT, REVEAL, LBRACKET, EXIT
+
+    defvar "IVT", IVT, 75 * 4
+
+    defcode "KEY?", KEYQ
+    mov r2, #0
+    ldr r0, =UART1
+    ldr r1, [r0, #UART_SR]
+    ands r3, #32
+    beq 1f
+    mvn r2, #1
+1:  push {r2}
+    NEXT
+
+    defvar "SBUF", SBUF, 16
+    defvar "SBUF-HEAD", SBUF_HEAD
+    defvar "SBUF-TAIL", SBUF_TAIL
+    defvar "UART0-TASK", UARTZ_TASK
+
+    .ltorg
